0538e78e494e355c1caf001ce43f381a03039144
Gabriel Zonatelle Borges <gabrielzonatelle@gmail.com>
2025-06-04 08:19:04 -0300

    Ajustes de erros

--- campuscrib/backend/microservices/registration-service/src/main/java/com/campuscrib/registration_service/infra/providers/JwtEmailConfirmationTokenProvider.java
+++ campuscrib/backend/microservices/registration-service/src/main/java/com/campuscrib/registration_service/infra/providers/JwtEmailConfirmationTokenProvider.java
@@ -6,7 +6,7 @@ import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
-import lombok.Value;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
@@ -31,7 +31,7 @@ public class JwtEmailConfirmationTokenProvider implements EmailConfirmationToken
     @Override
     public String generateToken(UUID userId) {
         Date now = new Date();
-        Date expiryDate = new Date(now.getTime() + EXPIRATION_MILLIS);
+        Date expiryDate = new Date(now.getTime() + expirationMillis);
 
         return Jwts.builder()
                 .setSubject(userId.toString())
