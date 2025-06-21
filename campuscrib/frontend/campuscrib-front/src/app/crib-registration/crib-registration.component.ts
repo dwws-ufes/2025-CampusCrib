8ba75a170e14781fbe9f0cf772115af463080942
Rosiane Da Silva Sangali <rosianesangali@gmail.com>
2025-06-21 18:22:29 -0300

    Criação Componente Crib-registration

---
+++ campuscrib/frontend/campuscrib-front/src/app/crib-registration/crib-registration.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crib-registration',
+  standalone: true,
+  imports: [],
+  templateUrl: './crib-registration.component.html',
+  styleUrl: './crib-registration.component.css'
+})
+export class CribRegistrationComponent {
+
+}
