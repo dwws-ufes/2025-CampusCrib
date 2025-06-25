2728656641be13e6fb92caa5bc7082cffe650f93
Gabriel Zonatelle Borges <gabrielzonatelle@gmail.com>
2025-06-25 00:44:27 -0300

    starting crib-manager

---
+++ campuscrib/backend/microservices/crib-manager-service/src/main/java/com/campuscrib/crib_manager_service/CribManagerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.campuscrib.crib_manager_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CribManagerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CribManagerServiceApplication.class, args);
+	}
+
+}
---
+++ campuscrib/backend/microservices/crib-manager-service/src/test/java/com/campuscrib/crib_manager_service/CribManagerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campuscrib.crib_manager_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CribManagerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
